0f8e84cd4b6ca5ccc2943d1296dbb9344fe77e6f
Andrew <andrewboug@hotmail.com>
2023-05-11 23:34:34 -0500

    added function templates to routine_activities.js

--- db/adapters/routine_activities.js
+++ db/adapters/routine_activities.js
@@ -1,1 +1,11 @@
 const client = require("../client");
+
+function getRoutineActivityById() {}
+
+function addActivityToRoutine() {}
+
+function updateRoutineActivity() {}
+
+function destroyRoutineActivity() {}
+
+function getRoutineActivitiesByRoutine() {}
