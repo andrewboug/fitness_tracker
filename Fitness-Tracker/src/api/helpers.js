b92e1450d8720fa603118ba1572d8e25206eefc0
Brandon <bbrungardt02@gmail.com>
2023-06-05 11:05:59 -0500

    stopped at auth prop problem

---
+++ Fitness-Tracker/src/api/helpers.js
@@ -0,0 +1,161 @@
+const BASE_URL = `https://fitnesstrac-kr.herokuapp.com/`;
+
+export async function registerUser(username, password) {
+  try {
+    const response = await fetch(`${BASE_URL}/users/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user: {
+          username,
+          password,
+        },
+      }),
+    });
+    const result = await response.json();
+    console.log("Result from register user: ", result);
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function fetchMe(token) {
+  try {
+    const response = await fetch(`${BASE_URL}/users/me`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const result = await response.json();
+    console.log("Result in fetchMe: ", result);
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function loginUser(username, password) {
+  try {
+    const response = await fetch(`${BASE_URL}/users/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user: {
+          username,
+          password,
+        },
+      }),
+    });
+    const result = await response.json();
+    console.log("Result from logged in user: ", result);
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function fetchActivies() {
+  try {
+    const response = await fetch(`${BASE_URL}/posts`);
+    const postsList = await response.json();
+    return postsList;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function createActivity(title, description, price, token) {
+  try {
+    const response = await fetch(`${BASE_URL}/posts`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        post: {
+          title,
+          description,
+          price,
+        },
+      }),
+    });
+
+    const result = await response.json();
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// export async function updatePost(token) {
+//   try {
+//     const response = await fetch(`${BASE_URL}/posts/${ROUTINE_ID}`, {
+//       method: "PATCH",
+//       headers: {
+//         "Content-Type": "application/json",
+//         Authorization: `Bearer ${token}`,
+//       },
+//       body: JSON.stringify({
+//         post: {
+//           title,
+//           description,
+//           price,
+//           location,
+//           willDeliver,
+//         },
+//       }),
+//     });
+//     const result = await response.json();
+//     console.log(result);
+//     return result;
+//   } catch (err) {
+//     console.error(err);
+//   }
+// }
+
+// export async function deletePost(token, POST_ID) {
+//   try {
+//     const response = await fetch(`${BASE_URL}/posts/${POST_ID}`, {
+//       method: "DELETE",
+//       headers: {
+//         "Content-Type": "application/json",
+//         Authorization: `Bearer ${token}`,
+//       },
+//     });
+//     const result = await response.json();
+//     console.log(result);
+//     return result;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+// export async function messagePost(content, token, postId) {
+//   try {
+//     const response = await fetch(`${BASE_URL}/posts/${postId}/messages`, {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//         Authorization: `Bearer ${token}`,
+//       },
+//       body: JSON.stringify({
+//         message: {
+//           content,
+//         },
+//       }),
+//     });
+//     const result = await response.json();
+//     console.log(result);
+//     return result;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
---
+++ Fitness-Tracker/src/components/AuthForm.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import { registerUser, loginUser } from "../../api/users";
+import useAuth from "../../hooks/useAuth";
+import { useLocation } from "react-router-dom";
+
+export default function AuthForm() {
+  const { pathname } = useLocation();
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { setToken } = useAuth();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      let result;
+      if (pathname === "/login") {
+        result = await loginUser(username, password);
+      } else {
+        result = await registerUser(username, password);
+      }
+      console.log("Result in Component: ", result);
+      setToken(result.data.token);
+      localStorage.setItem("token", result.data.token);
+    } catch (error) {
+      console.error(error);
+    }
+    setUsername("");
+    setPassword("");
+  }
+
+  return (
+    <div>
+      <h1>Authorize User</h1>
+      <form className="gradient-border" id="box1" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="username"
+          placeholder="username"
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+        <input
+          type="text"
+          name="password"
+          placeholder="password"
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button id="button1">
+          {pathname === "/login" ? "Login" : "Register"}
+        </button>
+      </form>
+    </div>
+  );
+}
