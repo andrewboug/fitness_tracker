fc28806f42451f064e3a2d2b36545cc131c4474c
Andrew <andrewboug@hotmail.com>
2023-06-09 20:18:56 -0500

    Added all api helper functions

--- Fitness-Tracker/src/api/helpers.js
+++ Fitness-Tracker/src/api/helpers.js
@@ -60,29 +60,51 @@ export async function loginUser(username, password) {
   }
 }
 
-export async function fetchActivies() {
+export async function fetchMyActivies(token) {
   try {
-    const response = await fetch(`${BASE_URL}/posts`);
-    const postsList = await response.json();
-    return postsList;
-  } catch (error) {
-    console.error(error);
+    const response = await fetch(`${BASE_URL}/users/:username/routines`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const result = await response.json();
+    console.log(result);
+    return result;
+  } catch (err) {
+    console.error(err);
   }
 }
 
-export async function createActivity(title, description, price, token) {
+export async function fetchActivities() {
   try {
-    const response = await fetch(`${BASE_URL}/posts`, {
+    const response = await fetch(`${BASE_URL}/activities`, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const result = await response.json();
+
+    console.log(result);
+    return result;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+export async function createActivity(name, description, token) {
+  try {
+    const response = await fetch(`${BASE_URL}/activities`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${token}`,
       },
       body: JSON.stringify({
-        post: {
-          title,
+        activity: {
+          name,
           description,
-          price,
         },
       }),
     });
@@ -95,67 +117,183 @@ export async function createActivity(title, description, price, token) {
   }
 }
 
-// export async function updatePost(token) {
-//   try {
-//     const response = await fetch(`${BASE_URL}/posts/${ROUTINE_ID}`, {
-//       method: "PATCH",
-//       headers: {
-//         "Content-Type": "application/json",
-//         Authorization: `Bearer ${token}`,
-//       },
-//       body: JSON.stringify({
-//         post: {
-//           title,
-//           description,
-//           price,
-//           location,
-//           willDeliver,
-//         },
-//       }),
-//     });
-//     const result = await response.json();
-//     console.log(result);
-//     return result;
-//   } catch (err) {
-//     console.error(err);
-//   }
-// }
+export async function updateActivity(name, description, token) {
+  try {
+    const response = await fetch(`${BASE_URL}/activities`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      method: "PATCH",
+      body: JSON.stringify({
+        name,
+        description,
+      }),
+    });
 
-// export async function deletePost(token, POST_ID) {
-//   try {
-//     const response = await fetch(`${BASE_URL}/posts/${POST_ID}`, {
-//       method: "DELETE",
-//       headers: {
-//         "Content-Type": "application/json",
-//         Authorization: `Bearer ${token}`,
-//       },
-//     });
-//     const result = await response.json();
-//     console.log(result);
-//     return result;
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
+    const result = await response.json();
+    console.log(result);
+    return result;
+  } catch (err) {
+    console.error(err);
+  }
+}
 
-// export async function messagePost(content, token, postId) {
-//   try {
-//     const response = await fetch(`${BASE_URL}/posts/${postId}/messages`, {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//         Authorization: `Bearer ${token}`,
-//       },
-//       body: JSON.stringify({
-//         message: {
-//           content,
-//         },
-//       }),
-//     });
-//     const result = await response.json();
-//     console.log(result);
-//     return result;
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
+export async function getActivityRoutines() {
+  try {
+    const response = await fetch(
+      `${BASE_URL}/activities/:activityId/routines`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const result = await response.json();
+    console.log(result);
+    return result;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+export async function getRoutines() {
+  try {
+    const response = await fetch(`${BASE_URL}/routines`, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const result = await response.json();
+    console.log(result);
+    return result;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+export async function createRoutine(name, goal, isPublic, token) {
+  try {
+    const response = await fetch(`${BASE_URL}/routines`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        name,
+        goal,
+        isPublic,
+      }),
+    });
+    const result = await response.json();
+    console.log(result);
+    return result;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+export async function updateRoutine(name, goal, isPublic, token) {
+  try {
+    const response = await fetch(`${BASE_URL}/routines/:routineId`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        name,
+        goal,
+        isPublic,
+      }),
+    });
+    const result = await response.json();
+    console.log(result);
+    return result;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+export async function deleteRoutine(token) {
+  try {
+    const response = await fetch(`${BASE_URL}/routines/:routineId`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const result = await response.json();
+    console.log(result);
+    return result;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+export async function addActivityToRoutine(activityId, count, duration) {
+  try {
+    const response = await fetch(`${BASE_URL}/routines/:routineId/activities`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        activityId,
+        count,
+        duration,
+      }),
+    });
+    const result = await response.json();
+    console.log(result);
+    return result;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+export async function updateRoutineActivity(count, duration, token) {
+  try {
+    const response = await fetch(
+      `${BASE_URL}/routine_activities/:routineActivityId`,
+      {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          count,
+          duration,
+        }),
+      }
+    );
+    const result = await response.json();
+    console.log(result);
+    return result;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+export async function removeActivityfromRoutine(token) {
+  try {
+    const response = await fetch(
+      `${BASE_URL}/routine_activities/:routineActivityId`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    const result = await response.json();
+    console.log(result);
+    return result;
+  } catch (err) {
+    console.error(err);
+  }
+}
--- Fitness-Tracker/src/main.jsx
+++ Fitness-Tracker/src/main.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-browser-router";
 import App from "./App.jsx";
 import "./index.css";
 import AuthProvider from "./components/AuthProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
